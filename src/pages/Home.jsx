ac5fe9f2f48ba70833c86d47ca0f27dbe0f95622
Ricardo Mendez <Ricardojmendezn@gmail.com>
2022-09-11 18:35:57 -0300

    Tercer commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
+import Menu from '../components/Menu'
 
 const Home = () => {
     return (
-        <div> ¡React Shop! </div>
+        <><Header />
+        <Menu /></>
 
     );
 }    
